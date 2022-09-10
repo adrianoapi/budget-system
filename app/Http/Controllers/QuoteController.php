f70fc506f5add08ebe8795c9e90c213c8f026847
Adriano Araújo Costa <adrianoapi@hotmail.com>
2022-09-10 09:21:36 -0300

    update name generator

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -239,6 +239,19 @@ class QuoteController extends UtilController
 
     public function nameGenerate($companyName, $clientUF, $quoteID, $clientName)
     {
+       $arrClient  = explode(" ", $clientName);
+       $clientName = NULL;
+       $i=0;
+       foreach($arrClient as $value):
+        
+        $i++;
+        $clientName .= "{$value} ";
+
+        if($i >= 3){
+            break;
+        }
+       endforeach;
+
        $companyName = substr($companyName, 0, 3);
        return "{$companyName} - {$clientUF} - {$quoteID} - {$clientName}";
     }
