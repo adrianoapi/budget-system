e0575ad44d94f9b30812494dda06f51d1fd08542
Adriano Araújo Costa <adrianoapi@hotmail.com>
2022-09-11 13:16:42 -0300

    fixed issue sum frete

--- resources/views/quotes/itemsTable.blade.php
+++ resources/views/quotes/itemsTable.blade.php
@@ -170,7 +170,7 @@
                 <p>
                     <span class="light">Desconto</span>
                     <span class="totalprice">
-                        <?php $total = $total - $quote->total; ?>
+                        <?php $total = ($total+$quote->frete) - $quote->total; ?>
                         R${{number_format($total, 2, ",",".")}}
                     </span>
                 </p>
