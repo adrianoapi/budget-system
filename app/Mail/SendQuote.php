7f329badfed038fbb8b9d5d9bbd0b6516e236f8d
Adriano Araújo Costa <adrianoapi@hotmail.com>
2022-09-17 14:28:57 -0300

    udpate mail from

--- app/Mail/SendQuote.php
+++ app/Mail/SendQuote.php
@@ -30,6 +30,6 @@ class SendQuote extends Mailable
      */
     public function build()
     {
-        return $this->from('contato@maniadeganhar.com.br')->subject("Orçamento Aprovado")->view('mail.quote');
+        return $this->from(getenv('MAIL_FROM_ADDRESS'))->subject("Orçamento Aprovado")->view('mail.quote');
     }
 }
