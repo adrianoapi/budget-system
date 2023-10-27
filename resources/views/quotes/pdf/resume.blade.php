8c99c2d424b8d03ad54a1ba7b6f6477f9fa4fcd1
Adriano Araújo Costa <adrianoapi@hotmail.com>
2023-10-27 18:34:27 -0300

    add image base 64 2

--- resources/views/quotes/pdf/resume.blade.php
+++ resources/views/quotes/pdf/resume.blade.php
@@ -138,7 +138,7 @@ style="float:right;margin-top:-80px;margin-right:-45px;z-index:-1;" />
 								@if(!empty($logo))
 								<?php 
 								$imgsrc = '@'.base64_encode(route('usuarios.image.show', ['logo' => $logo])); 
-								echo $html = '<img src="'.$imgsrc.'" width="300" height="60" border="0" />';
+								echo $html = '<img src="data:image/png;base64,'.$imgsrc.'" width="300" height="60" border="0" />';
 								?>
 								@else
 									<a href="http://www.dryairtec.com.br" style="color:#333333 !important; font-size:16px; font-family: Arial, Verdana, sans-serif; padding-left:10px;">www.dryairtec.com.br</a>
